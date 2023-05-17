4dabdb6894ad8e45aba75cd88f1bd6bbfc5ddc12
indra-18 <indraind0918@gmail.com>
2023-05-17 10:35:59 +0530

    User and Video Models defined

---
+++ server/models/video.model.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+
+const VideoSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: String,
+        required: true
+    },
+    duration: {
+        type: String,
+        required: true
+    },
+    views: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    visibility: {
+        type: String,
+        required: true
+    }
+})
+
+const VideoModel = mongoose.model('Videos', VideoSchema)
+module.exports = VideoModel
