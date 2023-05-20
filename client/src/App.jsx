be541993c43e0088d51c4f2985011862501b855d
indra-18 <indraind0918@gmail.com>
2023-05-21 00:32:52 +0530

    created VideoContext and contextProvider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import AppRouter from './router/appRouter'
+import VideoContextProvider from './contextApi/VideoContextApi'
 
 const App = () => {
   return (
-    <>
+    <VideoContextProvider>
       <AppRouter />
-    </>
+    </VideoContextProvider>
   )
 }
 
