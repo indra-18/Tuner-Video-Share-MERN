3533780403dc6972d313fcef92e5e568c9ef2197
indra-18 <indraind0918@gmail.com>
2023-05-21 00:35:01 +0530

    searchList page completed, search functionality in Navbar and modified route for videopage

--- client/src/components/searchListPage/SearchedList.jsx
+++ client/src/components/searchListPage/SearchedList.jsx
@@ -1,37 +1,38 @@
-import React from "react";
+import React, { useContext} from "react";
 import Navbar from "../Navbar";
-import userIcon from '../../assets/userIcon.jpg';
-import playIcon from '../../assets/playIcon.png'
+import userIcon from "../../assets/userIcon.jpg";
+import playIcon from "../../assets/playIcon.png";
+import { Link } from "react-router-dom";
+import { VideoContext } from "../../contextApi/VideoContextApi";
 
-const SearchedList = () => (
-  <div>
-    {/* <Navbar /> */}
-    {/* <div className="grid grid-cols-12 gap-2 gap-y-4 max-w-6xl">
-      <div className="col-span-12 sm:col-span-6 md:col-span-3"> */}
-    <div className="flex flex-col relative w-[210px] h-[140px]">
-      <div className="relative">
-        <img
-          src="https://picsum.photos/seed/59/312/201"
-          className="w-full h-full rounded-[10px]"
-        />
-        <img
-          src={userIcon}
-          className="absolute bottom-2 right-2 rounded-full max-h-[30px] max-w-[30px]"
-        />
-        <img src={playIcon} className="absolute top-2 right-2 rounded-full max-h-[30px] max-w-[30px]" />
-      </div>
-      <div className="absolute left-1 bottom-2 text-gray-200 text-[8px] mt-1">20 May 2023</div>
-      <div className="absolute bottom-2 left-14 text-gray-200 text-[8px] px-1 py">4 min</div>
-      <div className="absolute bottom-2 left-24 text-gray-200 text-[8px] mt-1">241K views</div>
+const SearchedList = () => {
+  const { searchResults } = useContext(VideoContext)
+
+  return (
+    <div className="bg-[#0F121FF5] w-full h-screen">
+      <Navbar />
+      <ul className=" m-8 grid grid-cols-4 gap-4">
+        {searchResults.map((card) => (
+          <li key={card._id} className="flex items-center">
+            <Link to={`/searchlist/video/${card._id}`}>
+              <div className="relative w-full h-40">
+                <video className="w-full h-full object-cover rounded">
+                  <source src={card.video} type="video/mp4" />
+                  Your browser does not support the video tag.
+                </video>
+                <img src={userIcon} className="absolute bottom-2 right-2 rounded-full max-h-6 max-w-6" />
+                <img src={playIcon} className="absolute top-2 right-2 rounded-full max-h-6 max-w-6" />
+                <div style={{ maxWidth: "50%", overflowWrap: "break-word", wordWrap: "break-word", wordBreak: "break-word" }} className=" absolute left-1 bottom-12 text-white font-bold text-justify text-xs mt-1">{card.title}</div>
+                <div className="absolute left-1 bottom-2 text-gray-200 text-xs mt-1">{card.date}</div>
+                <div className="absolute bottom-2 left-14 text-gray-200 text-xs px-1 py">{card.duration}</div>
+                <div className="absolute bottom-2 left-24 text-gray-200 text-xs mt-1">{card.views}</div>
+              </div>
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
-    {/* </div>
-    </div> */}
-  </div>
-);
-{
-  /* <video className="w-96" controls>
-  <source src="" type="video/mp4">
-</video> */
-}
+  );
+};
 
 export default SearchedList;
--- client/src/router/appRouter.jsx
+++ client/src/router/appRouter.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from '../components/homePage/Home'
 import Register from '../components/registerPage/Register'
@@ -9,22 +9,20 @@ import SearchedList from '../components/searchListPage/SearchedList'
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-
 const AppRouter = () => {
-  
   return (
     <BrowserRouter>
-        <Routes>
-            <Route path='/' element={<Home />}/>
-            <Route path='/register' element={<Register />} />
-            <Route path='/login' element={<Login />}/>
-            <Route path='/myvideos' element={<MyVideosPage />}/>
-            <Route path='/searchlist' element={<SearchedList />}/>
-            <Route path='/video' element={<VideoPage />}/>
-        </Routes>
-        <ToastContainer theme="dark" />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/myvideos' element={<MyVideosPage />} />
+        <Route path='/searchlist' element={<SearchedList />} />
+        <Route path='/searchlist/video/:videoId' element={<VideoPage />} />
+      </Routes>
+      <ToastContainer theme="dark" />
     </BrowserRouter>
-  )
+  );
 }
 
-export default AppRouter
+export default AppRouter;
