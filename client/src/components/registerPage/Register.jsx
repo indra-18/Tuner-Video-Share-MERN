0a5220f346e304cb71f2fe26831631e0eab00075
indra-18 <indraind0918@gmail.com>
2023-06-12 05:50:58 +0530

    fix bugs in login.jsx and delete hero.jsx

--- client/src/components/registerPage/Register.jsx
+++ client/src/components/registerPage/Register.jsx
@@ -1,5 +1,4 @@
 import React,{useState} from 'react'
-import Hero from '../Hero'
 import { NavLink } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import { useNavigate } from "react-router-dom";
