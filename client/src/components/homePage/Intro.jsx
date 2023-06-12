90d00dfaf53588639634565bca7f20e8e9241481
Indra <113177091+indra-18@users.noreply.github.com>
2023-06-12 05:40:39 +0530

    Revert "Arti"

--- client/src/components/homePage/Intro.jsx
+++ client/src/components/homePage/Intro.jsx
@@ -46,49 +46,61 @@ const Content = () => {
     } else {
       setName("View all");
     }
-    return (<>
-        <div className={`banner${banner}`} id="height">
-          
-            {!toggle?<>
-            <img src="https://s1.dmcdn.net/v/QQG701T4FrD60Fc9W/x1080" onClick={()=>setToggle(true)} className="img1" alt=""/> 
-            <div className="details">
-                <h1>{data.slice(0,1).map((data,key)=>{return data.title})}</h1>
-                <div className="span">
-                    <span>21 May 2023 </span>
-                    <span>Last 1 hour </span>
-                    <span>186k Views </span>
-                </div>
-            </div></>:
-            <>
-             <video src={data.slice(0,1).map((data,key)=>{return data.video})} onClick={()=>setToggle(false)} controls autoPlay/>
-             </>
-            }
+  };
 
-            
-        </div>   
+  return (
+    <>
+      <div className={`banner${banner}`} id="height">
+        {!toggle ? (
+          <img
+            src="https://s1.dmcdn.net/v/QQG701T4FrD60Fc9W/x1080"
+            onClick={() => setToggle(true)}
+            className="img1"
+            alt=""
+          />
+        ) : (
+          <video
+            src={data.slice(0, 1).map((data, key) => {
+              return data.video;
+            })}
+            onClick={() => setToggle(false)}
+            controls
+            autoPlay
+          />
+        )}
 
-        <div className="video-content">
-            <div className="btns">
-                <div className="recent">
-                    Recent
-                </div>
-                <div className="view-all" onClick={handleViewButton}>
-                    {name}
-                </div>
-            </div>
-            <div className="videos">            
-                {!banner? data.slice(0,5).map((data,key) => {
-                       return <Card data={data} videoData={videoData}/>
-                    }):
-                    data.map((data) => {
-                       return <Card data={data} videoData={videoData}/>
-                    })
-                }
-            </div>
+        <div className="details">
+          <h1>
+            {data.slice(0, 1).map((data, key) => {
+              return data.title;
+            })}
+          </h1>
+          <div className="span">
+            <span>21 May 2023</span>
+            <span>Last 1 hour</span>
+            <span>186k Views</span>
+          </div>
+        </div>
+      </div>
 
+      <div className="video-content">
+        <div className="btns">
+          <div className="recent">Recent</div>
+          <div className="view-all" onClick={handleViewButton}>
+            {name}
+          </div>
+        </div>
+        <div className="videos">
+          {!banner
+            ? data.slice(0, 4).map((data) => {
+                return <Card key={data._id} data={data} videoData={videoData} />;
+              })
+            : data.map((data) => {
+                return <Card key={data._id} data={data} videoData={videoData} />;
+              })}
         </div>
       </div>
     </>
   );
-}};
+};
 export default Content;
