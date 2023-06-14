ffdcbe1598c62452897147f4f6a5d756dd437f08
indra-18 <indraind0918@gmail.com>
2023-06-14 09:35:14 +0530

    add loader to intro.jsx and list.jsx

--- client/src/components/homePage/Intro.jsx
+++ client/src/components/homePage/Intro.jsx
@@ -3,6 +3,7 @@ import Card from "./card";
 import "./Intro.css";
 import { getAllVideos } from "../../services/nodeApi";
 import { VideoContext } from "../../contextApi/VideoContextApi";
+import Loader from "../Loader";
 
 const Content = () => {
   const [banner, setBanner] = useState("");
@@ -95,18 +96,7 @@ const Content = () => {
             </ul>
           </div>
         </div>
-      ) : (
-        <div className="w-full h-auto flex justify-center items-center align-middle">
-          <div
-            class=" text-green-600 inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] text-success motion-reduce:animate-[spin_1.5s_linear_infinite]"
-            role="status"
-          >
-            <span class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
-              Loading...
-            </span>
-          </div>
-        </div>
-      )}
+      ) : <Loader />}
     </>
   );
 };
