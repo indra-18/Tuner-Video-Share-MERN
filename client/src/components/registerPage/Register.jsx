caaf9ecb37b5c5ea47b14c49ada4df641e6fc5aa
indra-18 <indraind0918@gmail.com>
2023-06-15 10:46:12 +0530

    fix password visibility issue in register page

--- client/src/components/registerPage/Register.jsx
+++ client/src/components/registerPage/Register.jsx
@@ -116,21 +116,26 @@ const Register = () => {
               onChange={(e) => setPhone(e.target.value)} required />
       </div>
       
-      <div className="flex items-center border-b-2 py-2 px-3 mb-4">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
-          <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd" />
-        </svg>
-        <input className="pl-2 outline-none border-none bg-gray-800 text-white" type="text" name="password" id="Password" placeholder="Password" value={password}
-              onChange={(e) => setPassword(e.target.value)} required/>
-      </div>
-      <div className="flex items-center border-b-2 py-2 px-3 mb-6">
-      <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
-          <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd" />
-        </svg>
-
-        <input className="pl-2 outline-none border-none bg-gray-800 text-white" type="text" name="confirmPassword" id="Password" placeholder="Confirm Password" value={confirmPassword}
-              onChange={(e) => setConfirmPassword(e.target.value)} required />
-      </div>
+      <div className="flex items-center border-b-2 mb-4 py-2 px-3 ">
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
+            <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd" />
+          </svg>
+          <input className="pl-2 w-full outline-none border-none bg-gray-800 text-white" type="password" name="password" id="password" placeholder="Password" 
+            onChange={(e)=>{
+            setPassword(e.target.value)
+          }} value={password}
+          />
+        </div>
+        <div className="flex items-center border-b-2 mb-4 py-2 px-3 ">
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
+            <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd" />
+          </svg>
+          <input className="pl-2 w-full outline-none border-none bg-gray-800 text-white" type="password" name="password" id="password" placeholder="Confirm Password" 
+            onChange={(e)=>{
+              setConfirmPassword(e.target.value)
+          }} value={confirmPassword}
+          />
+        </div>
       <button type="submit" className="block w-full bg-purple-400 mt-4 py-2 rounded-2xl text-white font-semibold mb-2 hover:bg-indigo-800">Register</button>
     </form>
   </div>
