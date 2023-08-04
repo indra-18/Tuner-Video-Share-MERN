7cbcef04b6aedbc3bada5075a7269a3ae2395a7a
indra-18 <indraind0918@gmail.com>
2023-08-04 13:48:53 +0530

    replace all card related code and use VideoCard component

--- client/src/components/homePage/Intro.jsx
+++ client/src/components/homePage/Intro.jsx
@@ -1,10 +1,11 @@
 import React, { useContext, useEffect, useState } from "react";
-import Card from "./card";
 import "./Intro.css";
 import { getAllVideos } from "../../services/nodeApi";
 import { VideoContext } from "../../contextApi/VideoContextApi";
 import Loader from "../Loader";
 import HomeCarousel from "./HomeCarousel";
+import VideoCard from "../VideoCard";
+import { Link } from "react-router-dom";
 
 const Content = () => {
   const [banner, setBanner] = useState("");
@@ -40,6 +41,14 @@ const Content = () => {
     }
   };
 
+  const getCard = (card) => (
+    <li key={card._id} className="flex items-center">
+      <Link to={`/home/video/${card._id}`}>
+        <VideoCard card={card} />
+      </Link>
+    </li>
+  )
+
   return (
     <>
       <HomeCarousel />
@@ -55,11 +64,11 @@ const Content = () => {
             <ul 
             className=" ml-8 mr-8 mb-8  gap-4 block sm:grid sm:grid-cols-[repeat(auto-fit,minmax(200px,1fr))] sm:gap-4 ">
               {!banner
-                ? allVideos.slice(0, 4).map((data) => {
-                    return <Card key={data._id} data={data} />;
+                ? allVideos.slice(0, 4).map((card) => {
+                    return getCard(card);
                   })
-                : allVideos.map((data) => {
-                    return <Card key={data._id} data={data} />;
+                : allVideos.map((card) => {
+                    return getCard(card);
                   })}
             </ul>
           </div>
--- client/src/components/searchListPage/SearchedList.jsx
+++ client/src/components/searchListPage/SearchedList.jsx
@@ -1,10 +1,9 @@
 import React, { useContext } from "react";
 import Navbar from "../Navbar";
-import userIcon from "../../assets/userIcon.jpg";
-import playIcon from "../../assets/playIcon.png";
-import { Link } from "react-router-dom";
 import { VideoContext } from "../../contextApi/VideoContextApi";
 import Upload from "../Upload";
+import { Link } from "react-router-dom";
+import VideoCard from "../VideoCard";
 
 const SearchedList = () => {
   const { searchResults } = useContext(VideoContext);
@@ -15,46 +14,11 @@ const SearchedList = () => {
       <div className="relative">
         <ul className=" m-8  gap-4 block sm:grid sm:grid-cols-[repeat(auto-fit,minmax(200px,1fr))] sm:gap-4">
           {searchResults.map((card) => (
-            <li key={card._id} className="flex items-center max-sm:mb-4">
-              <Link to={`/searchlist/video/${card._id}`}>
-                <div className="relative w-full h-auto">
-                  <video className="w-full h-full object-cover rounded hover:border-[#C4B4F8] hover:border-4">
-                    <source src={card.video} type="video/mp4" />
-                    Your browser does not support the video tag.
-                  </video>
-                  <img
-                    src={userIcon}
-                    className="absolute bottom-2 right-2 rounded-full max-h-6 max-w-6"
-                  />
-                  <img
-                    src={playIcon}
-                    className="absolute top-2 right-2 rounded-full max-h-6 max-w-6"
-                  />
-                  <div
-                    style={{
-                      maxWidth: "50%",
-                      overflowWrap: "break-word",
-                      wordWrap: "break-word",
-                      wordBreak: "break-word",
-                    }}
-                    className=" absolute left-1 bottom-12 text-white font-bold text-justify text-xs mt-1"
-                  >
-                    {card.title}
-                  </div>
-                  <div className="absolute bottom-2 left-2 flex justify-start items-baseline">
-                    <div className="ml-2 text-gray-200 text-xs mt-1">
-                      {card.date}
-                    </div>
-                    <div className="ml-2 text-gray-200 text-xs px-1 py">
-                      {card.duration} Mins
-                    </div>
-                    <div className="ml-2 text-gray-200 text-xs mt-1">
-                      {card.views} Views
-                    </div>
-                  </div>
-                </div>
-              </Link>
-            </li>
+                  <li key={card._id} className="flex items-center">
+                  <Link to={`/searchlist/video/${card._id}`}>
+                    <VideoCard card={card} />
+                  </Link>
+                </li>          
           ))}
         </ul>
         <Upload />
